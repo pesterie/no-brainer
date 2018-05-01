0ed2859d51bf9331132976fbc6d5ad56755c7c9c
Pierre Estérie <pesterie@gmail.com>
2018-05-01 16:52:00 +0200

    Adding gpio files

---
+++ include/components/gpio.hpp
@@ -0,0 +1,10 @@
+//=============================================================================
+// Author : Pierre Esterie
+// This is free folks !
+//=============================================================================
+#ifndef INCLUDE_COMPONENTS_GPIO_HPP
+#define INCLUDE_COMPONENTS_GPIO_HPP
+
+#include <wiringPi.h>
+
+#endif
