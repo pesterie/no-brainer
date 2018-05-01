b81e2d25c872510666c7661d150aff551d7d3eef
Pierre Estérie <pesterie@gmail.com>
2018-05-01 16:50:33 +0200

    Adding setup scripts

---
+++ setup-desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "==================================================="
+echo " Setup script for desktop "
+echo "==================================================="
+
+# installing libraries needed on the desktop to train the car
+
---
+++ setup-pi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "==================================================="
+echo " Setup script for the pi "
+echo "==================================================="
+
+# installing libraries needed on the pi to control/train the car
+
+# You also need to :
+#  - download the Boost Libraries
+#  - install wiringPi
+
+apt-get install cmake
+apt-get install g++
